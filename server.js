74b6852dc5211051ed25055870817fac89b3f35d
Giani <g.goras@redsharp.ro>
2020-05-04 18:12:49 +0300

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) =>
-  res.json({ msg: "welcome to the contact keeper api" })
-);
+//connect database
+connectDB();
+
+//init middleware
+app.use(express.json({ extended: false }));
+
+// app.get("/", (req, res) =>
+//   res.json({ msg: "welcome to the contact keeper api" })
+// );
 
 //define routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+//serve static assets in production
+if (process.env.NODE_ENV !== "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
